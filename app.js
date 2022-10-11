5b9a47de50e8d6b3502b3d1e405caa993c54e80c
Thoinoo <thoino@outlook.fr>
2022-10-11 17:25:42 +0200

    utilisation jquery, utilisation des slides

--- app.js
+++ app.js
@@ -2,21 +2,17 @@
 import * as ressources from './ressources.module.js';
 
 
-const main = document.getElementById('main');
-const loading = document.getElementById('loading');
 
-load();
+
+$(document).ready(function () {
+    load();
+});
+
+
 
 
 async function load_json_file() {
 
-    /*
-    let result = await fetch('./etudiants.json');
-    let jsonData = await result.json();
-    let promise = await load_students_via_api(jsonData.students)
-*/
-
-    // fonctionel result undefined
     let result = await fetch('./etudiants.json')
         .then(data => data.json())
         .then(data => load_students_via_api(data.students))
@@ -27,11 +23,11 @@ async function load_json_file() {
 }
 
 async function load() {
-    main.style.visibility = 'hidden';
+    jQuery("#main").css("visibility", "hidden");
     var result = await load_json_file();
 
     loading.style.display = 'none';
-    main.style.visibility = 'visible';
+    jQuery("#main").css("visibility", "visible");
     console.log('terminé');
 }
 
@@ -45,11 +41,7 @@ async function load_students_via_api(students) {
         });
         let jsonData = await result.json();
         addDiv(jsonData);
-
     }
-
-
-
 }
 
 
@@ -57,51 +49,53 @@ async function load_students_via_api(students) {
 
 
 async function addDiv(student) {
-    console.log('addDiv');
 
-    let d = document.createElement('div');
-    d.id = student.id;
-
-    let entete = document.createElement('div');
-    entete.className = "flexColumnCenter";
-    d.appendChild(entete);
-
-    let nom = document.createElement('a');
-    entete.appendChild(nom);
-    nom.text = student.login;
-    nom.href = student.html_url;
-    nom.className = 'pseudo';
-    nom.classList.add('overUnderLined', 'letterSpace5');
+    jQuery('<div>', {
+        id: student.id,
+        class: 'case'
+    }).appendTo('#main');
 
 
-    let avatar_url_link = document.createElement('a');
-    entete.appendChild(avatar_url_link);
-    avatar_url_link.href = student.html_url;
-
-    let avatar_url = document.createElement('img');
-    avatar_url.src = student.avatar_url;
-    avatar_url_link.appendChild(avatar_url);
-
-    let public_repos = document.createElement('p');
-    d.appendChild(public_repos);
-    public_repos.innerHTML = "Nombre de repos : " + student.public_repos;
-
-    let followers = document.createElement('p');
-    d.appendChild(followers);
-    followers.innerHTML = "followers   : " + student.followers;
+    jQuery('<div>', {
+        id: student.id + "_entete",
+        class: "flexColumnCenter"
+    }).appendTo('#' + student.id);
 
 
-    let bio = document.createElement('p');
-    d.appendChild(bio);
+    jQuery('<a>', {
+        id: student.id + "_name",
+        text: student.login,
+        href: student.html_url,
+        class: 'pseudo'
+    }).appendTo('#' + student.id + "_entete");
+
+
+
+
+    jQuery('<a>', {
+        id: student.id + "_avatar_url_link",
+        href: student.html_url
+    }).appendTo('#' + student.id + "_entete");
+
+
+    jQuery('<img>', {
+        id: student.id + "_avatar_url",
+        src: student.avatar_url
+    }).attr('data-html_url', student.html_url).appendTo('#' + student.id + "_avatar_url_link");
+
+    //document.getElementById(student.id + "_avatar_url").alt = ;
+
+
+    jQuery('<p>').appendTo('#' + student.id).html("Nombre de repos : " + student.public_repos);
+
+    jQuery('<p>').appendTo('#' + student.id).html("followers   : " + student.followers)
+
+
     if (student.bio == null) {
-        bio.innerHTML = "Bio : Wow ! So empty !";
+        jQuery('<p>').html("Bio : Wow ! So empty !").appendTo('#' + student.id);
     } else {
-        bio.innerHTML = "Bio : " + student.bio;
+        jQuery('<p>').html("Bio : " + student.bio).appendTo('#' + student.id);
     }
-
-
-    d.className = "case";
-    document.getElementById("main").appendChild(d);
 
 
 
@@ -117,76 +111,68 @@ async function addDiv(student) {
     });
     let jsonData_repos = await result_repos.json();
     generateRepos(jsonData_repos, student.id);
-
-
-
-
-
 }
 
 
 function generateFollowers(followers, id) {
 
-    let parent = document.getElementById(id);
-
     if (followers.length > 0) {
 
-        let label = document.createElement('p');
-        label.innerHTML = "Follower(s) : ";
-        parent.appendChild(label);
+        jQuery('<button>', { id: id + '_button_followers' }).html("Follower(s)").appendTo('#' + id);
 
         for (let i = 0; i < followers.length; i++) {
-            //console.log(followers[i].login);
+            jQuery('<a>', {
+                id: id + "_follower_" + followers[i].id,
+                text: followers[i].login,
+                href: followers[i].html_url,
+                class: "dottedUnderline follower"
+            }).appendTo('#' + id);
+        }
 
-            let follower_link = document.createElement('a');
-            follower_link.text = followers[i].login;
-            follower_link.href = followers[i].html_url;
-            follower_link.classList.add('dottedUnderline', "follower");
 
-            parent.appendChild(follower_link);
+        $('#' + id + '_button_followers').click(function () {
+            if ($('#' + id + "_follower_" + followers[0].id).is(":visible") == true) {
+                for (let i = 0; i < followers.length; i++) {
+                    $('#' + id + "_follower_" + followers[i].id).slideUp();
+                }
+            } else {
+                for (let i = 0; i < followers.length; i++) {
+                    $('#' + id + "_follower_" + followers[i].id).slideDown();
+                }
+            }
+        });
 
+
+        for (let i = 0; i < followers.length; i++) {
+            $('#' + id + "_follower_" + followers[i].id).slideUp();
         }
     }
 }
 
 async function generateRepos(repos, id) {
 
-
-
     if (repos.length > 0) {
 
-        let parent = document.getElementById(id);
         let list_repos = [];
 
         repos.forEach(element => {
             list_repos.push({ name: element.name, html_url: element.html_url, pushed_at: Date.parse(element.pushed_at) });
         });
 
-
-
-        // console.log(id);
-        // list_repos.forEach(repo => {console.log(repo.name + " / " + repo.pushed_at );});
         list_repos = SortByDate(list_repos, id)
-        //list_repos = list_repos.sort((a , b) => b.pushed_at - a.pushed_at);
-        // console.log("--------------------------------------")
-        //list_repos.forEach(element => {console.log(element.name + " / " + element.pushed_at) ;});
 
 
+        jQuery('<p>').html("Dernier(s) projets mis à jour : ").appendTo('#' + id);
 
 
-        let label = document.createElement('p');
-        label.innerHTML = "Dernier(s) projets mis à jour : ";
-        parent.appendChild(label);
 
         for (let i = 0; i < list_repos.length && i < 3; i++) {
-            //console.log(repos[i].login);
 
-            let repos_link = document.createElement('a');
-            repos_link.text = list_repos[i].name;
-            repos_link.href = list_repos[i].html_url;
-            repos_link.classList.add('dottedUnderline', "follower");
-
-            parent.appendChild(repos_link);
+            jQuery('<a>', {
+                text: list_repos[i].name,
+                href: list_repos[i].html_url,
+                class: "dottedUnderline follower"
+            }).appendTo('#' + id);
 
         }
     }
@@ -204,11 +190,6 @@ function SortByDate(list, id) {
 }
 
 
-
-function loadStudents() {
-
-
-}
 
 
 
